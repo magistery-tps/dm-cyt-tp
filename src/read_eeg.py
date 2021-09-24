3ec4ebad1b2c504912c8d68625d5523cd701cfd8
ffelicioni <ffelicioni@fi.uba.ar>
2021-09-24 16:59:32 -0300

    se saco order_asc_by_subject

--- src/read_eeg.py
+++ src/read_eeg.py
@@ -8,8 +8,7 @@ from eeg import EEG
 def load_eeg_dataset(path, srate = 128, verbose=False):
     return [load_eeg(file_path, srate, verbose) for file_path in glob.glob(path)]
 
-def order_asc_by_subject(eggs):
-    return sorted(eegs, key=lambda eeg: (int(eeg.subject),int(eeg.resting_state)))
+
 
 def to_data_frame(eegs):
     return pd.DataFrame([eeg.to_dict() for eeg in eegs], columns = eegs[0].to_dict().keys())
@@ -37,3 +36,4 @@ def load_eeg(file_path, srate = 128, verbose=False):
         filename_parts[1], 
         filename_parts[3]
     )
+
