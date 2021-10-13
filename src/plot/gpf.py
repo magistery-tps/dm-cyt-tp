9a15567d8999bcff6837cd45c76a0f19fc0de388
ffelicioni <ffelicioni@fi.uba.ar>
2021-10-13 15:25:43 -0300

    se agrega grafica_sil

--- src/plot/gpf.py
+++ src/plot/gpf.py
@@ -17,3 +17,66 @@ def plot_gfp(gfp, peaks, sfrequency, figsize=(30, 8)):
     
     ax.set_ylabel('GCP')
     ax.set_xlabel('Time (s)')
+
+
+    
+#%% Grafico silhoutte
+def grafica_sil(n_clusters,sil,labels):
+    import matplotlib.cm as cm
+    # Create a subplot with 1 row and 2 columns
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    
+    # The 1st subplot is the silhouette plot
+    # The silhouette coefficient can range from -1, 1 but in this example all
+    # lie within [-0.1, 1]
+    ax1.set_xlim([-0.5, 1])
+    # The (n_clusters+1)*10 is for inserting blank space between silhouette
+    # plots of individual clusters, to demarcate them clearly.
+    ax1.set_ylim([0, len(labels) + (n_clusters + 1) * 10])
+
+    
+    # The silhouette_score gives the average value for all the samples.
+    # This gives a perspective into the density and separation of the formed
+    # clusters
+    silhouette_avg = np.mean(sil)
+    print("For n_clusters =", n_clusters,
+          "The average silhouette_score is :", silhouette_avg)
+    
+    # Compute the silhouette scores for each sample
+    sample_silhouette_values = np.array(sil)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        # Aggregate the silhouette scores for samples belonging to
+        # cluster i, and sort them
+        ith_cluster_silhouette_values = \
+            sample_silhouette_values[labels == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                          0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10  # 10 for the 0 samples
+
+    ax1.set_title("The silhouette plot.")
+    ax1.set_xlabel("The silhouette coefficient values")
+    ax1.set_ylabel("Cluster label")
+
+    # The vertical line for average silhouette score of all the values
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+   # ax1.set_xticks([-1,-0.5,-0.2,0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    plt.show()
