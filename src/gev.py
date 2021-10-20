e0aeb7fbfe21962324d1085752aced9c59b51b06
ffelicioni <ffelicioni@fi.uba.ar>
2021-10-20 15:20:24 -0300

    cambio norm

--- src/gev.py
+++ src/gev.py
@@ -8,8 +8,8 @@ def calculo_gev(X, maps, n_ch, n_clusters, gfp, gfp2, L):
     n_t = data_norm.shape[0]
     # --- GEV ---
     maps_norm = maps - maps.mean(axis=1, keepdims=True)
-    maps_norm /= maps_norm.std(axis=1, keepdims=True)
-    #maps /= np.sqrt(np.sum(maps**2, axis=1, keepdims=True))
+    #maps_norm /= maps_norm.std(axis=1, keepdims=True)
+    maps_norm /= np.sqrt(np.sum(maps**2, axis=1, keepdims=True))
     # --- correlation data, maps ---
     C = np.dot(data_norm, maps_norm.T)/n_ch
     print("C.shape: " + str(C.shape[0])+str(C.shape[1]))
@@ -22,8 +22,8 @@ def calculo_gev(X, maps, n_ch, n_clusters, gfp, gfp2, L):
         gev[k] = np.sum(gfp[r]**2 * C[r,k]**2)/gfp2
     
     gev_sum=np.sum(gev)
-    print(f"\n[+] Global explained variance GEV = {gev_sum:.3f}")
-    for k in range(n_clusters):
-        print(f"GEV_{k:d}: {gev[k]:.3f}")
+    #print(f"\n[+] Global explained variance GEV = {gev_sum:.3f}")
+    #for k in range(n_clusters):
+    #    print(f"GEV_{k:d}: {gev[k]:.3f}")
     
-    return gev
+    return gev, gev_sum
