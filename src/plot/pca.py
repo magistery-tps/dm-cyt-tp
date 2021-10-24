1c9d668f5252814fccb3c2b67c81a42294ae896c
adrianmarino <adrianmarino@gmail.com>
2021-10-24 15:46:37 -0300

    feat: adjust pca params/style

--- src/plot/pca.py
+++ src/plot/pca.py
@@ -5,17 +5,17 @@ def plot_pca_df(df, title = '', target_column = 'label'):
     targets = np.sort(df.label.unique())
     fig = plt.figure(figsize = (6,6))
     ax = fig.add_subplot(1,1,1) 
-    ax.set_xlabel('pc1', fontsize = 15)
-    ax.set_ylabel('pc2', fontsize = 15)
-    ax.set_title(title, fontsize = 20)
-    colors = ['r', 'g', 'b', 'y']
-    for target, color in zip(targets, colors):
+    ax.set_xlabel('PC 1', fontsize = 15)
+    ax.set_ylabel('PC 2', fontsize = 15)
+    ax.set_title(title, fontsize = 12)
+
+    for target in targets:
         indicesToKeep = df[target_column] == target
         ax.scatter(
             df.loc[indicesToKeep, 'pc1'],
             df.loc[indicesToKeep, 'pc2'],
-            c = color,
-            s = 50
+            label=target,
+            s = 10
         )
-    ax.legend(targets)
+    ax.legend(targets, title='Clusters')
     ax.grid()
